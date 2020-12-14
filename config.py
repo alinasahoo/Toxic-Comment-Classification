833206e4977e846005ae047526ffca6b6465ff05
imalinasahoo <alinasahoo09@gmail.com>
2020-12-14 14:33:27 +0530

    Final Commit

---
+++ config.py
@@ -0,0 +1,22 @@
+'''
+configuration
+'''
+
+toxic_words = [
+	'asshole',
+	'douche',
+	'douchebag',
+	'moron',
+	'idiot',
+	'stupid',
+	'shit',
+	'schmuck',
+	'dickhead',
+	'jackass',
+	'ass',
+	'suck',
+	'sucking',
+	'idiotic',
+	'moronic',
+	'fuck',
+]
---
+++ ml_model/predict.py
@@ -0,0 +1,54 @@
+
+
+# modules
+import os
+import sys
+
+# add current directory to sys.path
+# needed for Flask app to work
+current_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(current_dir)
+
+# custom modules
+from train_classifier import Preprocess  # need to import the Preprocess class as reference for unpickling
+from utils import get_root, load_pipeline, get_logger
+
+
+ROOT_DIR = get_root()
+MODEL_PATH = os.path.join(ROOT_DIR, 'assets', 'model')
+PREPROCESSOR_FILE = os.path.join(MODEL_PATH, 'preprocessor.pkl')
+ARCHITECTURE_FILE = os.path.join(MODEL_PATH, 'gru_architecture.json')
+WEIGHTS_FILE = os.path.join(MODEL_PATH, 'gru_weights.h5')
+
+
+class PredictionPipeline(object):
+
+    def __init__(self, preprocessor, model):
+        self.preprocessor = preprocessor
+        self.model = model
+
+    def predict(self, text):
+        features = self.preprocessor.transform_texts(text)
+        pred = self.model.predict(features)
+        return pred
+
+
+if __name__ == "__main__":
+
+    logger = get_logger()
+    logger.info("Script Started")
+    logger.info("Loading model...")
+    ppl = PredictionPipeline(*load_pipeline(PREPROCESSOR_FILE, 
+                                            ARCHITECTURE_FILE, 
+                                            WEIGHTS_FILE))
+    logger.info("Completed loading model!")
+
+    sample_text = ['Corgi is stupid',
+                   'good boy',
+                   'School of AI is awesome',
+                   'F**K']
+
+    for text, toxicity in zip(sample_text, ppl.predict(sample_text)):
+        print(f"{text}".ljust(25) + f"- Toxicity: {toxicity}")
+
+    logger.info("Script Completed")
